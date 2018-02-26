5a9e976810084ef68b5daea1b0b1099c8c82f8d0
Brian Chew <bhchew@ucdavis.edu>
2018-02-26 04:10:22 +0000

    Header file

---
+++ holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdarg.h>
+#include <unistd.h>
+
+
+typedef struct milk
+{
+	char *id;
+	void (*f)(va_list);
+} milk_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+void write_cs(va_list);
+void write_per(va_list);
+
+#endif
