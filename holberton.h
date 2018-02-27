380c0f8363d00333269f0e4d1584c5c2283d2216
bchen528 <bchen720@gmail.com>
2018-02-27 04:07:16 +0000

    header file

--- holberton.h
+++ holberton.h
@@ -2,6 +2,7 @@
 #define TEA
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 /**
  * struct milk - structure for chars, strings, %
  * @id: character representing datatype or special character %
@@ -20,6 +21,7 @@ typedef struct milk
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_cs(va_list);
+int print_c(va_list);
+int print_s(va_list);
 int print_per(va_list);
 #endif /*TEA*/
