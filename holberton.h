cbb512a23180e3c3e6ce74db71968c03e67865e1
bchen528 <bchen720@gmail.com>
2018-02-27 04:01:10 +0000

    update prototypes

--- holberton.h
+++ holberton.h
@@ -6,7 +6,7 @@
  * struct milk - structure for chars, strings, %
  * @id: character representing datatype or special character %
  * @f: print function for specific datatype/character
- *
+ * 
  * Description: struct milk contains characters c, s, %
  * that represent datatypes/special character and function
  * pointers that point to a putchar function for
@@ -15,11 +15,11 @@
 typedef struct milk
 {
 	char *id;
-	void (*f)(va_list);
+	int (*f)(va_list);
 } milk_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-void print_cs(va_list);
-void print_per(va_list);
+int print_cs(va_list);
+int print_per(va_list);
 #endif /*TEA*/
